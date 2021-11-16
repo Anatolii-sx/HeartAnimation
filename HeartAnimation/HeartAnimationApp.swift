2abde63a94534e01efe968d67b2120056808817b
Anatolii-sx <s8.8@bk.ru>
2021-11-16 22:50:07 +0300

    Initial Commit

---
+++ HeartAnimation/HeartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartAnimationApp.swift
+//  HeartAnimation
+//
+//  Created by Анатолий Миронов on 16.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct HeartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
