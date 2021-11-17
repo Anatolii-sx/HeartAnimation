fa194ef2c7239d0029019897cfba848b65594db1
Anatolii-sx <s8.8@bk.ru>
2021-11-17 07:34:28 +0300

    Added slider

--- HeartAnimation/ContentView.swift
+++ HeartAnimation/ContentView.swift
@@ -12,47 +12,61 @@ struct ContentView: View {
     @State private var showAward = false
     
     // Main Size of Big Heart
-    private let size: CGFloat = 180
+    @State private var size: Double = 180
     
     var body: some View {
         
         ZStack {
-            
             Color(.black)
                 .ignoresSafeArea()
-            
-            SmallHeartView(showAward: showAward, size: size, color: .green)
-                .offset(x: showAward ? 0 : 0, y: showAward ? -size * 0.625 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .purple)
-                .offset(x: showAward ? -size * 0.625 : 0, y: showAward ? 0 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .yellow)
-                .offset(x: showAward ? size * 0.625 : 0, y: showAward ? 0 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .orange)
-                .offset(x: showAward ? 0 : 0, y: showAward ? size * 0.625 : 0)
-            
-            SmallHeartView(showAward: showAward, size: size, color: .red)
-                .offset(x: showAward ? -size * 0.45 : 0, y: showAward ? -size * 0.45 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .blue)
-                .offset(x: showAward ? size * 0.45 : 0, y: showAward ? -size * 0.45 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .purple)
-                .offset(x: showAward ? size * 0.45 : 0, y: showAward ? size * 0.45 : 0)
-            SmallHeartView(showAward: showAward, size: size, color: .green)
-                .offset(x: showAward ? -size * 0.45 : 0, y: showAward ? size * 0.45 : 0)
+            VStack {
+                Spacer()
+                ZStack {
+                    SmallHeartView(showAward: showAward, size: size, color: .green)
+                        .offset(x: showAward ? 0 : 0, y: showAward ? -size * 0.625 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .purple)
+                        .offset(x: showAward ? -size * 0.625 : 0, y: showAward ? 0 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .yellow)
+                        .offset(x: showAward ? size * 0.625 : 0, y: showAward ? 0 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .orange)
+                        .offset(x: showAward ? 0 : 0, y: showAward ? size * 0.625 : 0)
+                    
+                    SmallHeartView(showAward: showAward, size: size, color: .red)
+                        .offset(x: showAward ? -size * 0.45 : 0, y: showAward ? -size * 0.45 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .blue)
+                        .offset(x: showAward ? size * 0.45 : 0, y: showAward ? -size * 0.45 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .purple)
+                        .offset(x: showAward ? size * 0.45 : 0, y: showAward ? size * 0.45 : 0)
+                    SmallHeartView(showAward: showAward, size: size, color: .green)
+                        .offset(x: showAward ? -size * 0.45 : 0, y: showAward ? size * 0.45 : 0)
 
-            ZStack {
-                HeartView()
-                    .frame(width: size, height: size)
-                    .foregroundColor(.yellow)
-                
-                HeartView()
-                    .frame(width: size, height: size)
-                    .scaleEffect(showAward ? 1 : 0)
-                    .foregroundColor(showAward ? .red : .white)
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.3)) {
-                            showAward.toggle()
-                        }
+                    ZStack {
+                        HeartView()
+                            .frame(width: size, height: size)
+                            .foregroundColor(.yellow)
+                        
+                        HeartView()
+                            .frame(width: size, height: size)
+                            .scaleEffect(showAward ? 1 : 0)
+                            .foregroundColor(showAward ? .red : .white)
+                            .onTapGesture {
+                                withAnimation(.easeInOut(duration: 0.3)) {
+                                    showAward.toggle()
+                                }
+                            }
                     }
+                }
+                
+                Spacer()
+                
+                Slider(value: $size, in: 40...200, step: 1)
+                    .padding()
+                
+                Text("Tap the heart")
+                    .foregroundColor(.white)
+                    .font(.system(size: 24))
+                    .padding(.bottom, 32)
+                    
             }
         }
     }
